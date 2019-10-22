381443af18f357963b9d3d46c82393e766890633
Asier <Asier@DESKTOP-V53S952>
2019-10-22 20:40:28 +0200

    Departamentua txt y csv

--- DatuAtzipenaethazi/src/main/java/kontroladorea/Menukontroladorea.java
+++ DatuAtzipenaethazi/src/main/java/kontroladorea/Menukontroladorea.java
@@ -1,6 +1,10 @@
 package kontroladorea;
 
 import java.io.BufferedWriter;
+<<<<<<< Upstream, based on branch 'master' of https://github.com/asier58/Ethazi_datuAtzipena.git
+=======
+
+>>>>>>> 22d88e3 Departamentua txt y csv
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,6 +17,7 @@ import lehioa.Menua;
 public class Menukontroladorea {
 	public static Logger logger = Logger.getLogger(Menukontroladorea.class);
 	private Menua menua;
+<<<<<<< Upstream, based on branch 'master' of https://github.com/asier58/Ethazi_datuAtzipena.git
 	private Departamentua departamentua;
 <<<<<<< Upstream, based on branch 'master' of https://github.com/asier58/Ethazi_datuAtzipena.git
 	
@@ -74,10 +79,19 @@ public class Menukontroladorea {
 	//Lehioak
 	//Hasierako Pantaila
 =======
+=======
+	private Departamentua departamentua;
+>>>>>>> 22d88e3 Departamentua txt y csv
 
+<<<<<<< Upstream, based on branch 'master' of https://github.com/asier58/Ethazi_datuAtzipena.git
 	// Lehioak
 	// Hasierako Pantaila
 >>>>>>> ac2480d Datu baserako konexioa FINAL
+=======
+	//Lehioak
+	//Hasierako Pantaila
+
+>>>>>>> 22d88e3 Departamentua txt y csv
 	public void nireMenua(Menua menua) {
 		this.menua = menua;
 
@@ -95,6 +109,7 @@ public class Menukontroladorea {
 		departamentua.setVisible(true);
 	}
 
+<<<<<<< Upstream, based on branch 'master' of https://github.com/asier58/Ethazi_datuAtzipena.git
 <<<<<<< Upstream, based on branch 'master' of https://github.com/asier58/Ethazi_datuAtzipena.git
 	public void parametroakHartu(String fitxategi, String formatua) {
 		File fitx = new File(fitxategi+formatua);
@@ -197,3 +212,106 @@ public class Menukontroladorea {
 =======
 }
 >>>>>>> ac2480d Datu baserako konexioa FINAL
+=======
+
+
+
+	public void parametroakHartu(String fitxategi, String formatua) {
+		File fitx = new File(fitxategi+formatua);
+		if (!fitx.exists()) {
+			logger.error("Fitxategia ez da existitzen");
+			departamentua.erroreaAtera();
+			}
+		
+		else {
+			
+			if(formatua.equalsIgnoreCase(".txt")) {
+				eredua.departamentua.txtKudeatu(fitxategi,formatua);
+			}
+			
+			if(formatua.equalsIgnoreCase(".xml")) {
+				
+			}
+			
+			if(formatua.equalsIgnoreCase(".csv")) {
+				eredua.departamentua.csvkudeatu(fitxategi,formatua);
+			}
+			
+				
+			
+			
+			
+			
+			
+			
+			}
+		}
+
+	private void txtKudeatu(String fitxategi, String formatua) {
+			
+			
+		}
+	private void csvKudeatu(String fitxategi, String formatua) {
+		
+		
+	}
+	private void xmlKudeatu(String fitxategi, String formatua) {
+		
+		
+	}
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+
+>>>>>>> 22d88e3 Departamentua txt y csv
